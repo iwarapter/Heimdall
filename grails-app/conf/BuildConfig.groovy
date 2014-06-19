034bed2274875e6b936050e60903a3d812f0b53d
Sion Williams <sion5@hotmail.co.uk>
2014-06-19 10:20:45 +0100

    Upgraded to Grails 2.4.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -31,9 +31,6 @@ grails.project.dependency.resolution = {
     log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
     legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
-	
-	def gebVersion = "0.9.2"
-	def seleniumVersion = "2.32.0"
 
     repositories {
         inherits true // Whether to inherit repository definitions from plugins
@@ -43,32 +40,37 @@ grails.project.dependency.resolution = {
         mavenLocal()
         grailsCentral()
         mavenCentral()
-		
-		// For Geb snapshot
-		mavenRepo "http://oss.sonatype.org/content/repositories/snapshots"
+        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
     }
 
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
-		
+        // runtime 'mysql:mysql-connector-java:5.1.29'
+        // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
+        test "org.grails:grails-datastore-test-support:1.0-grails-2.4"
     }
 
     plugins {
         // plugins for the build system only
-        build ":tomcat:7.0.52.1"
+        build ":tomcat:7.0.54"
 
         // plugins for the compile step
-        compile ":scaffolding:2.0.3"
-        compile ':cache:1.1.2'
+        compile ":scaffolding:2.1.1"
+        compile ':cache:1.1.6'
+        compile ":asset-pipeline:1.8.11"
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate:3.6.10.13" // or ":hibernate4:4.3.5.1"
+        runtime ":hibernate4:4.3.5.4" // or ":hibernate:3.6.10.16"
         runtime ":database-migration:1.4.0"
-        runtime ":jquery:1.11.0.2"
-        runtime ":resources:1.2.7"
-		
-		// cobertura coverage
-		test ":code-coverage:1.2.7"
-		
+        runtime ":jquery:1.11.1"
+
+        // Uncomment these to enable additional asset-pipeline capabilities
+        //compile ":sass-asset-pipeline:1.7.4"
+        //compile ":less-asset-pipeline:1.7.0"
+        //compile ":coffee-asset-pipeline:1.7.0"
+        //compile ":handlebars-asset-pipeline:1.3.0.3"
     }
 }
--- grails-app/views/environment/calendar.gsp
+++ grails-app/views/environment/calendar.gsp
@@ -4,9 +4,12 @@
 			Calendar for ${environment.name}
 		</title>
 		<meta name="layout" content="main"/>
-        <r:require module="calendar" />
-		
-	</head>
+        <asset:stylesheet src="/fullcalendar/fullcalendar.css"/>
+        <asset:javascript src="/fullcalendar/lib/moment.min.js"/>
+        <asset:javascript src="/fullcalendar/fullcalendar.js"/>
+        <asset:javascript src="calendar.js"/>
+
+    </head>
 	<body>
 		<h1>Calendar for ${environment.name}</h1>
 		

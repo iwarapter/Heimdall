b43b53564edc49d7ccd23d908757588c829b4b6d
Sion Williams <sion5@hotmail.co.uk>
2014-06-20 15:13:46 +0100

    Added the calendar link to show.gsp. This allows you to see the corresponding calendar for the environment.

--- grails-app/controllers/com/willis/heimdall/EnvironmentController.groovy
+++ grails-app/controllers/com/willis/heimdall/EnvironmentController.groovy
@@ -1,24 +1,108 @@
 package com.willis.heimdall
 
+import grails.converters.JSON
 import org.joda.time.DateTime
 
-import grails.converters.JSON
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
 import java.text.SimpleDateFormat
 
 /**
  * Environment Controller
  * @author Sion Williams
  */
+@Transactional(readOnly = true)
 class EnvironmentController {
 
-    BookingService bookingService
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
-    def scaffold = true
-	
-	def calendar = {
-		def environment = Environment.findById(params.id)
-		[ environment : environment ]
-	}
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Environment.list(params), model: [environmentInstanceCount: Environment.count()]
+    }
+
+    def show(Environment environmentInstance) {
+        respond environmentInstance
+    }
+
+    def create() {
+        respond new Environment(params)
+    }
+
+    @Transactional
+    def save(Environment environmentInstance) {
+        if (environmentInstance == null) {
+            notFound()
+            return
+        }
+
+        if (environmentInstance.hasErrors()) {
+            respond environmentInstance.errors, view: 'create'
+            return
+        }
+
+        environmentInstance.save flush: true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'environment.label', default: 'Environment'), environmentInstance.id])
+                redirect environmentInstance
+            }
+            '*' { respond environmentInstance, [status: CREATED] }
+        }
+    }
+
+    def edit(Environment environmentInstance) {
+        respond environmentInstance
+    }
+
+    @Transactional
+    def update(Environment environmentInstance) {
+        if (environmentInstance == null) {
+            notFound()
+            return
+        }
+
+        if (environmentInstance.hasErrors()) {
+            respond environmentInstance.errors, view: 'edit'
+            return
+        }
+
+        environmentInstance.save flush: true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'Environment.label', default: 'Environment'), environmentInstance.id])
+                redirect environmentInstance
+            }
+            '*' { respond environmentInstance, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Environment environmentInstance) {
+
+        if (environmentInstance == null) {
+            notFound()
+            return
+        }
+
+        environmentInstance.delete flush: true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Environment.label', default: 'Environment'), environmentInstance.id])
+                redirect action: "index", method: "GET"
+            }
+            '*' { render status: NO_CONTENT }
+        }
+    }
+
+    def calendar = {
+        def environment = Environment.findById(params.id)
+        [ environment : environment ]
+    }
 
     def bookingList = {
         def environment = Environment.findById(params.id)
@@ -50,6 +134,16 @@ class EnvironmentController {
             json {
                 render eventList as JSON
             }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'environment.label', default: 'Environment'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*' { render status: NOT_FOUND }
         }
     }
 }
